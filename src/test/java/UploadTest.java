788b1bfe4f4aa1afd6979301e647cbe94c1184d1
Lilit <hayrapetyanlilit@gmail.com>
2018-01-25 15:31:13 +0400

    upload page and test

--- src/test/java/UploadTest.java
+++ src/test/java/UploadTest.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import org.testng.annotations.Test;
+import pages.UploadPage;
 
-public class UploadTest {
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.testng.Assert.assertTrue;
+
+public class UploadTest extends BaseTest {
+
+    @Test
+    public void uploadFile() throws IOException {
+        Path filePath  = Files.createTempFile("testFile",".txt");
+        File file = filePath.toFile();
+        String path = file.getAbsolutePath();
+        UploadPage uploadPage = new UploadPage();
+        uploadPage.typePath(path);
+        uploadPage.submit();
+
+        assertTrue(uploadPage.getUploadedFiles().contains(file.getName()),  "File was not uploaded!");
+    }
 }
+
